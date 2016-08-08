76856371955be38c571afdfd26dbed048a632e93
Elliot Wright <wright.elliot@gmail.com>
2016-08-08 21:07:41 +0100

    Some code examples, updates to initial slides.

---
+++ code/01-assignments-type-inference.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+// START OMIT
+func main() {
+	// Method 1
+	var foo string
+
+	foo = "Hello, World!"
+
+	// Method 2
+	bar := "Hello, World!"
+
+	fmt.Println(foo)
+	fmt.Println(bar)
+}
+
+// END OMIT

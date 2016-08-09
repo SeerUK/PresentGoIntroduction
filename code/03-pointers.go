8697fd36f45ea7f8b71e85c40856bea707ce7ff9
Elliot Wright <wright.elliot@gmail.com>
2016-08-09 22:10:53 +0100

    Pointers!

--- code/03-pointers.go
+++ code/03-pointers.go
@@ -4,7 +4,19 @@ import "fmt"
 
 // START OMIT
 func main() {
-	fmt.Println("Hello, World!")
+	n1, n2 := 42, 84
+
+	p1 := &n1           // Create a pointer to n1
+	fmt.Println(1, p1)  // Print the pointer
+	fmt.Println(2, *p1) // Print the underlying value (dereference)
+
+	*p1 = 21           // Set n1 through the pointer
+	fmt.Println(3, n1) // Print the new value of n1
+
+	p2 := n2
+	p2 = 42
+	fmt.Println(4, p2) // What will these be?
+	fmt.Println(5, n2) // ...
 }
 
 // END OMIT

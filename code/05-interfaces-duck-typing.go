c9e0f52134695a740bd93d1a327e05718f063fa0
Elliot Wright <wright.elliot@gmail.com>
2016-08-09 22:45:56 +0100

    Goroutines, and channels.

--- code/05-interfaces-duck-typing.go
+++ code/05-interfaces-duck-typing.go
@@ -3,8 +3,22 @@ package main
 import "fmt"
 
 // START OMIT
+type Greeter interface {
+	Greet(string) string
+}
+
+type HelloGreeter struct{}
+
+func (g HelloGreeter) Greet(name string) string {
+	return fmt.Sprintf("Hello, %s!", name)
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	var greeter Greeter
+
+	greeter = HelloGreeter{}
+
+	fmt.Println(greeter.Greet("Byng"))
 }
 
 // END OMIT
--- code/06-goroutines.go
+++ code/06-goroutines.go
@@ -1,10 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 // START OMIT
+func delayedReturn(in string, delay time.Duration, ch chan string) {
+	time.Sleep(delay * time.Millisecond)
+
+	ch <- in
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	ch := make(chan string)
+
+	go delayedReturn("World", 2000, ch)
+	go delayedReturn("Hello...", 200, ch)
+
+	fmt.Println("Waiting for return...")
+
+	fmt.Print(<-ch)
+	fmt.Println(<-ch)
 }
 
 // END OMIT

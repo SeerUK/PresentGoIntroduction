c5d5dbdf61109ce2e3594dae9573c7ed645b9725
Elliot Wright <wright.elliot@gmail.com>
2016-08-09 22:21:39 +0100

    Structs!

--- code/04-structs.go
+++ code/04-structs.go
@@ -3,8 +3,21 @@ package main
 import "fmt"
 
 // START OMIT
+type Greeter struct {
+	Name string
+}
+
+// Capitalisation of names is important...
+func (g *Greeter) Greet() string {
+	return fmt.Sprintf("Hello, %s!", g.Name)
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	greeter := Greeter{
+		Name: "Byng",
+	}
+
+	fmt.Println(greeter.Greet())
 }
 
 // END OMIT

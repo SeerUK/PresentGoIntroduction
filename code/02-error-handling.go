f40c1bb6bd11be76c9d4e33d604fd3c7252b05ad
Elliot Wright <wright.elliot@gmail.com>
2016-08-09 21:23:32 +0100

    Error handling.

--- code/02-error-handling.go
+++ code/02-error-handling.go
@@ -1,10 +1,26 @@
 package main
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
+// START OMIT
 func main() {
+	// notVeryUsefulFunction returns a function if you ask for one.
+	err := notVeryUsefulFunction(true)
+	if err != nil {
+		// Perhaps we should try not making an error...
+		fmt.Println("Oops, lets just pretend that never happened...")
+	}
 
+	err = notVeryUsefulFunction(false)
+	if err != nil {
+		panic(err)
+	}
 }
+
+// END OMIT
 
 func notVeryUsefulFunction(shouldError bool) error {
 	if shouldError {
